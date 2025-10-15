cf4af3511dbb395c0eba9610a163acb8342c90be
Srijha <srijhakalyan99@gmail.com>
2025-10-14 23:31:24 -0400

    changes

--- Problem1_dynamic_tabular_approach.py
+++ Problem1_dynamic_tabular_approach.py
@@ -14,7 +14,7 @@ class Solution(object):
 
 
         for j in range(1, m+1):
-            dp[0][j] = -1
+            dp[0][j] = 99999
         
         for i in range(1, n+1):
             for j in range(1, m+1):
